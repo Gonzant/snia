127f4aa18d1c68c89ef9becfae5d074b9e0941c9
cbarreira <cbarreira@NB-DACC-SNIA04.mgap.gub.uy>
2016-06-20 09:03:11 -0300

    mejoras de estilos

---
+++ public_html/visores/coneat_1/nls/snianls.js
@@ -0,0 +1,98 @@
+define({
+    "root": ({
+        "widgets": {
+            "MapasBaseWidget": {
+                rbVectorial: "Vectorial",
+                rbImagen: "Imagen"
+            },
+            "CoordenadasWidget": {
+                lbUTM: "UTM21S/WGS84  ",
+                lbGEO: "Geog/WGS84    ",
+                lbWM:  "Web Mercator  "
+            },
+            "BuscarWidget": {
+                lbID2: "Nro Padrón(*):",
+                bBuscar: "Buscar",
+                lbdepartamento: "Departamento",
+                lbAcercarSeleccion: "Acercar a la Seleccion",
+                lbRemoverResultado: "Remover Seleccion",
+                lbSeleccionarTodo: "Seleccionar Todo",
+                lbCroquisSeleccion: "Croquis de Selección",
+                lbCubrimiento: "Muestra el porcentaje de superficie del padrón cubierto por los grupos coneat",
+                lbBuscando: "Buscando...",
+                lbIngresePadron:"Por favor ingrese un número de padrón.",
+                lbEncontro: "Se encontro",
+                lbEncontraron: "Se encontraron",
+                lbElemento: "elemento",
+                lbElementos: "elementos",
+                lbErrorBuscar: "Error al buscar.",
+                lbErrorNoSeleccionCroquis: "Se necesita al menos un elemento seleccionado para el croquis.",
+                lbErrorNoSeleccionCubrimiento: "Se necesita al menos un elemento seleccionado para el cubrimiento.",
+                lbMantenerGeo: "Mantener geometría"
+                
+            },
+            "NavegarWidget": {
+                tbAlejar: "Alejar",
+                tbAcercar: "Acercar",
+                tbDesplazar: "Desplazar",
+                tbExtensionMaxima: "Extensión Máxima",
+                tbExtensionAnterior: "Vista Anterior",
+                tbExtensionPosterior: "Vista Posterior"
+            },
+            "ImprimirWidget": {
+                lbTemplateSelect: "Formato",
+                lbEscala: "Escala"                
+            },
+            "EstilosWidget": {
+                tTabMenu: "Menu",
+                tTabBotones: "Botones",
+                tTabTexto: "Texto",
+                tTabTree: "Tree",
+                tTabComboBox:"ComboBox",
+                tTabRadioButtonYCheckBox:"RadioButton/CheckBox",
+                tTabTabs:"Tabs",
+                tTabTogglebutton:"Togglebutton",
+                tTabTextArea:"TextArea",
+                tTabNumberSpinner:"NumberSpinner",
+                tTabOtros: "Otros",
+                tTabTextBox: "TextBox"
+            },
+            "IdentificarWidget": {
+                lbidentificando: "Idenificando...",
+                lbNoCapas: "No hay capas visibles",
+                lbElementos: "elementos",
+                lbSeIdentificaron: "Se identificaron",
+                lbErrorIdentificar: "Error al identificar.",
+                lbClicIdentificar: "Clic en el mapa  para identificar"
+            },
+            "AyudaWidget": {
+                
+            },
+            "AreasConeatWidget": {
+                lbMantenerGeo: "Mantener geometría"   
+            },
+            "MedirWidget": {
+                lbArea: "Area",
+                lbDistancia: "Distancia",
+                lbm2:"Metros Cuadrados",
+                lbkm2:"Kilómetros cuadrados",
+                lbha:"Hectáreas",
+                lbm:"Metros",
+                lbkm:"Kilómetros",
+                lbmi: "Millas"
+            },
+            "InformacionSuelosWidget": {
+                lbordenar: "Ordenar por",
+                lbagrupar: "Agrupar por",
+                lbAcercarUnidadMapeo: "Acercar a Unidad de Mapeo seleccionada",
+                lbAcercarPerfil: "Acercar a Perfil seleccionado",
+                lbSinElementos: "No se encontró ningun elemento",
+                lbLimpiar: "Limpiar Selección"
+            },            
+            "ContenidosWidget": {
+                bColapsar: "Colapsar",
+                bExpandir: "Expandir"
+            }
+        }
+    })
+});
