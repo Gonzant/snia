11f8c2a1801ba13f69217d90464084413b6a22c3
Danilo da Rosa <daniloide@gmail.com>
2016-08-11 13:02:57 -0300

    Ahora el nombre del campo es "layers". Ejemplo de configuración donde se define que subcapas se ven. 	 {                 "url": "http://web.renare.gub.uy/arcgis/rest/services/BASE/Administrativo/MapServer",                 "options": {                         "id": "Administrativo",                         "opacity": 0.7,                         "visible": false                 },                 "layers": [0,1]             },

--- public_html/visores/js/snia/app.js
+++ public_html/visores/js/snia/app.js
@@ -48,10 +48,10 @@ snia.app = {
                 var dynLayers = mapaConfig.mapa.dynamicLayers;
                 arrayUtil.forEach(dynLayers, function (dataLayer, index) {
                     var l = new ArcGISDynamicMapServiceLayer(dataLayer.url, dataLayer.options);
-                    if (dataLayer.subLayers){
+                    if (dataLayer.layers){
                         l.on("load", function (e) {
                             e.target._tocInfos = [];
-                            arrayUtil.forEach(dataLayer.subLayers, function (layerInfoIndex) {
+                            arrayUtil.forEach(dataLayer.layers, function (layerInfoIndex) {
                                 e.target._tocInfos.push(e.target.layerInfos[layerInfoIndex]);
                             });
                         });
