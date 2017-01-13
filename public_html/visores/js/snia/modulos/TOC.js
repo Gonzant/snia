321ecc12e3a9fde18a9b4df218e7788d65b9161d
Danilo da Rosa <daniloide@gmail.com>
2017-01-13 05:35:35 -0300

    Nuevo visor "prueba" para probar contenidos nuevo

--- public_html/visores/js/snia/modulos/TOC.js
+++ public_html/visores/js/snia/modulos/TOC.js
@@ -240,8 +240,9 @@ define([
                     sublayerTooltip = "";
                 }
                 if (!dataLayer.layers || arrayUtil.indexOf(dataLayer.layers, li.id) >= 0) {
-                    if (li.parentLayerId >= 0) { //Si es una sub-capa de segundo nivel
-                        tParent = l.layerInfos[li.parentLayerId].name;
+                    i = li.parentLayerId;
+                    if (i >= 0) { //Si es una sub-capa de segundo nivel
+                        tParent = l.layerInfos[i].name;
                     }
                     this._data.push({ id: li.name, name: li.name, index: li.id, tooltip: sublayerTooltip, type: 'layer', maxScale: li.maxScale, minScale: li.minScale, parent:  tParent, vparent: vparent, startChecked: li.defaultVisibility });
                     //this._borrarGruposDeVisibleLayers(l, li);
