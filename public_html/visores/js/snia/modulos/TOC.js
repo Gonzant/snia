7d2ab57776548d3e2063336d396bc237c7551e47
Danilo da Rosa <daniloide@gmail.com>
2017-03-28 16:30:15 -0300

    Agregado de scroll horizontal para poder ver nombres largos

--- public_html/visores/js/snia/modulos/TOC.js
+++ public_html/visores/js/snia/modulos/TOC.js
@@ -77,7 +77,8 @@ define([
         _init: function () {
             this._generarData();
             this._crearTree();
-            domStyle.set(this._tree.domNode, "overflow", "hidden");
+            domStyle.set(this._tree.domNode, "overflow-x", "auto");
+            domStyle.set(this._tree.domNode, "overflow-y", "hidden");
         },
         _executeGP: function (url) {
             this._gpXMLInfo = new Geoprocessor(this.proxyXML2JSON);
